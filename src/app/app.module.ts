7b45a334cd960f115850fc513c2605a5526603c1
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2017-10-04 13:32:27 -0300

    concluindo carrinho de compras e localização de moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -39,7 +39,7 @@ import { ReviewsComponent } from './restaurante-detail/reviews/reviews.component
     RouterModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [RestaurantService, ShoppingCartService],
+  providers: [RestaurantService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurante-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurante-detail/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { CartItem } from './cart-item.model';
+import { MenuItem } from '../menu-item/menu-item.model';
+
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html'
@@ -12,11 +15,23 @@ export class ShoppingCartComponent implements OnInit {
   }
 
   items(){
-    return this.shoppingCartService.items
+    return this.shoppingCartService.items;
+  }
+
+  clear(){
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item: CartItem) {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: MenuItem){
+    this.shoppingCartService.addItem(item);
   }
 
   total(){
-    return this.shoppingCartService.total()
+    return this.shoppingCartService.total();
   }
 
 }
