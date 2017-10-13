34d21a36a79c1d69c84f09aab31012829d4a1daf
Rodrigo Oliveira <rodrigooliveira@rodrigo.local>
2017-10-13 08:58:03 -0300

    concluindo refactory do input e adicionando componente de lista de itens (com bug) em order.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,16 @@ import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { RestaurantComponent } from './restaurantes/restaurant/restaurant.component'
 import { RestaurantService } from './restaurantes/restaurant/restaurant.service';
 import { ShoppingCartService } from './restaurante-detail/shopping-cart/shopping-cart.service';
+import { OrderService } from './order/order.service'
 import { RestauranteDetailComponent } from './restaurante-detail/restaurante-detail.component';
 import { MenuItemComponent } from './restaurante-detail/menu-item/menu-item.component';
 import { ShoppingCartComponent } from './restaurante-detail/shopping-cart/shopping-cart.component';
 import { MenuComponent } from './restaurante-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurante-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 
 @NgModule({
@@ -34,7 +38,10 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent,
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +50,7 @@ import { OrderComponent } from './order/order.component';
     RouterModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [RestaurantService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [RestaurantService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import {NgModel} from '@angular/forms'
 
 @Component({
-  selector: 'app-input',
+  selector: 'app-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
+  
+  input: any
+  
+  @Input() label: string
+  @Input() errorMessage: string
+
+  @ContentChild(NgModel) model: NgModel
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(){
+    this.input = this.model
+    if(this.input === undefined){
+      throw new Error('Precisa ser usado com uma diretiva')
+    }
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched)
+  }
+
+  hasError(): boolean{
+    return this.input.invalid && (this.input.dirty || this.input.touched)
   }
 
 }
