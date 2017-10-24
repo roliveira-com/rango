acc9c1e83568610956d52ebd634b6b5b04552286
Rodrigo Oliveira <rodrigooliveira@rodrigo.home>
2017-10-24 08:23:00 -0200

    animações de restaurantes, items de menu e itens no carrinho usando o estado void

--- src/app/restaurante-detail/menu-item/menu-item.component.ts
+++ src/app/restaurante-detail/menu-item/menu-item.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 import { MenuItem } from './menu-item.model'
 import { ShoppingCartService } from '../shopping-cart/shopping-cart.service'
 
 @Component({
   selector: 'app-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemIn',[
+      state('ready', style({opacity:1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready';
 
   @Input() menuItem: MenuItem
   @Output() add = new EventEmitter()
--- src/app/restaurantes/restaurant/restaurant.component.ts
+++ src/app/restaurantes/restaurant/restaurant.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 import { Restaurant } from './restaurant.model'
 
 @Component({
   selector: 'app-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantIn',[
+      state('ready', style({opacity:1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready';
 
   @Input() restaurant: Restaurant
 
