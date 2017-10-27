1e193c64cf105b2b1a14db5a0bb29c195732d6a2
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2017-10-27 14:46:28 -0200

    evitando multiplos requests na busca

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -5,7 +5,13 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import { Restaurant } from './restaurant/restaurant.model';
 import {RestaurantService} from './restaurant/restaurant.service';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-restaurantes',
@@ -46,7 +52,19 @@ export class RestaurantesComponent implements OnInit {
       searchInput: this.searchInput
     });
 
-    this.searchInput.valueChanges.switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm))
+    this.searchInput.valueChanges
+      // O operador rxjs debounceTime() se certifica que eventos com intervalos
+      // maiores que o tempo espcificado entre um e o outro não sejam considerados.
+      // Neste caso, é possível evitar multiplos requests ao servidor a cada letra digitada
+      .debounceTime(500)
+      // Já o operador distinctUntilChanged() desconsidera se o valor digitado
+      // for o mesmo que o anterior. Neste caso, é possível evitar requests
+      // ao servidor caso o termo digitado for igual ao da busca anterior
+      .distinctUntilChanged()
+      // O uso do catch() em restaurants() evita que um eventual erro disparado pelo servidor
+      // na chamada http quebre o observable(). Desta forma, tratamos o erro para que,
+      // ao invés do erro em si, ele retorne um array vazio.
+      .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm).catch(error=>Observable.from([])))
       .subscribe(restaurants => this.restaurants = restaurants);
 
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
